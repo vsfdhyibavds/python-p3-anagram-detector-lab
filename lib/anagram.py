79f79b8c54e10947f30dcadc0f1baec8a8706e8e
Eugene Wekesa <eugenco578@gmail.com>
2025-05-22 22:57:52 +0300

    init and class methods

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        sorted_word = sorted(self.word)
+        return [candidate for candidate in candidates if sorted(candidate.lower()) == sorted_word]
